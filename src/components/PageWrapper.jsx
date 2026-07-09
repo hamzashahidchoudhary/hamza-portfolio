61c4e7e33d814808be2ff9adda1d2105cee9bff5
Muhammad Hamza <hamzachoudhary9454@gmail.com>
2026-07-09 16:15:03 +0500

    page fix

--- src/components/PageWrapper.jsx
+++ src/components/PageWrapper.jsx
@@ -1,6 +1,14 @@
 import { motion } from 'framer-motion'
+import { useEffect } from 'react'
+import { useLocation } from 'react-router-dom'
 
 export default function PageWrapper({ children }) {
+  const { pathname } = useLocation()
+
+  useEffect(() => {
+    window.scrollTo(0, 0)
+  }, [pathname])
+
   return (
     <motion.div
       initial={{ opacity: 0, y: 18 }}
